ea5f35286811a9dbb9c2ddaa2fb252518e1b8a20
Alkawi <alexander.kaiser1996@web.de>
2022-02-07 15:47:39 +0100

    Create markup for PokemonListItem

--- src/Pokedex/PokemonListItem.tsx
+++ src/Pokedex/PokemonListItem.tsx
@@ -1,4 +1,6 @@
 import { useQuery } from "react-query";
+import { Link } from "react-router-dom";
+import styles from "./PokemonListItem.module.css";
 
 const PokemonListItem = ({ name, url }: { name: string; url: string }) => {
   const { data, isLoading } = useQuery(
@@ -8,7 +10,25 @@ const PokemonListItem = ({ name, url }: { name: string; url: string }) => {
     },
     { staleTime: 600_000 }
   );
-  return <></>;
+  return (
+    <>
+      {!isLoading && (
+        <Link to={`/details/${data.name}`} className={styles["item-container"]}>
+          <div>
+            <img src={data.sprites.front_default} alt="" />
+          </div>
+          <div className={styles["item-content"]}>
+            <div>
+              #{data.id} <strong>{data.name}</strong>
+            </div>
+            <div>
+              Types: {data.types.map((t: any) => t.type.name).join(", ")}
+            </div>
+          </div>
+        </Link>
+      )}
+    </>
+  );
 };
 
 export default PokemonListItem;
