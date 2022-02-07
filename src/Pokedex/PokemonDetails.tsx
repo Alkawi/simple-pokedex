0978533e33150ff71d09e88deaaafd3ab4c38946
Alkawi <alexander.kaiser1996@web.de>
2022-02-07 19:30:34 +0100

    Improve styling

--- src/Pokedex/PokemonDetails.tsx
+++ src/Pokedex/PokemonDetails.tsx
@@ -1,14 +1,28 @@
 import { useQuery } from "react-query";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { detailFetcher } from "../Api";
+import PokemonListItem from "./PokemonListItem/PokemonListItem";
+import styles from "./PokemonDetails.module.css";
 
 const PokemonDetails = () => {
   const { name } = useParams<{ name: string }>();
-  const { data, isLoading } = useQuery(["pokemon-detail", name], detailFetcher(name), {
-    staleTime: 600_000,
-  });
+  const { data, isLoading } = useQuery(
+    ["pokemon-detail", name],
+    detailFetcher(name),
+    {
+      staleTime: 600_000,
+    }
+  );
 
-  return <>{!isLoading && JSON.stringify(data)}</>;
+  return (
+    <>
+      <Link to="/" className={styles["nav-bar"]}>
+        &lt; Back to the Pokedex
+      </Link>
+      {!isLoading && <PokemonListItem name={name} url={data.url} />}
+    </>
+  );
 };
 
 export default PokemonDetails;
