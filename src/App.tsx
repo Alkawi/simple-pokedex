3b1feefebca79a1a1a7c091698d13e9ea3661978
Alkawi <alexander.kaiser1996@web.de>
2022-02-07 15:31:19 +0100

    Create routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import PokemonList from "./Pokedex/PokemonList";
-import { createBrowserHistory } from "history";
+import { Route, Routes } from "react-router";
+import PokemonDetails from "./Pokedex/PokemonDetails";
+import { BrowserRouter } from "react-router-dom";
 
-const history = createBrowserHistory();
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PokemonList />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <Routes>
+        <QueryClientProvider client={queryClient}>
+          <Route element={<PokemonList />} path="/" />
+          <Route element={<PokemonDetails />} path="/details/:name" />
+        </QueryClientProvider>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
