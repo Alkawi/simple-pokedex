078652a6df2c360b0978e9c9aedea61ce2a8cc8b
Alkawi <alexander.kaiser1996@web.de>
2022-02-07 15:14:25 +0100

    Create BrowserHistory

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import PokemonList from "./Pokedex/PokemonList";
+import { createBrowserHistory } from "history";
 
+const history = createBrowserHistory();
 const queryClient = new QueryClient();
 
 const App = () => {
