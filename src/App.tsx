5b4d3592d32acefaeeae500cc67138410f9932e4
Alkawi <alexander.kaiser1996@web.de>
2022-02-07 14:56:58 +0100

    Add PokemonList to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
+import PokemonList from "./Pokedex/PokemonList";
 
 const queryClient = new QueryClient();
 
 const App = () => {
-  return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PokemonList />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
